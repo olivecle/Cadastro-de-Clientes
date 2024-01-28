0c228ee968885ecc0991749766a1585896752399
Cleber Oliveira <cleber.oliveira@yahoo.com.br>
2024-01-28 01:56:54 -0300

    Atualizacao de Liberacao de Credito

--- cadastro1.js
+++ cadastro1.js
@@ -17,29 +17,17 @@ class cadastro {
     }    
 }
 
-const Joao = new cadastro('Joao Augusto', 'Rua Bragança 25 bairro: Bosque', 'Masculino', 2000, 34678290, 3752993450);
-console.log(Joao);
+const Jaqueline = new cadastro('Jaqueline Aparecido da Silva', 'Rua Brigadeiro Faria Lima 349 bairro: Centro', 'Feminino', 5000, 242342572, 4056790358);
+console.log(Jaqueline);
 
-const Jaqueline = new cadastro('Jaqueline Aparecido da Silva', 'Rua Brigadeiro Faria Lima 349 bairro: Centro', 'Feminino', 5000, 242342572, 4056790358)
-console.log(Jaqueline)
+const Fabio = new cadastro('Fabio Carlos de Andrade', 'Rua Antonio Figueiredo 98 bairro: São Benedito', 'Masculino', 1200, 345876346, 3490982348);
+console.log(Fabio);
 
 
-
-/*criarCadastro.nome = 'Joao Augusto';
-criarCadastro.endereco = 'Rua Bragança 25 bairro: Bosque';
-criarCadastro.sexo = 'M'
-criarCadastro.renda = 1300;
-criarCadastro.numeroRg = 34678290;
-criarCadastro.numeroCpf = 3752993450;
-
-if (criarCadastro.renda > 1500) {
+if (this.renda >= 1500) {
     console.log('Crédito Liberado');
 }
 
 else {
     console.log('Crédito não liberado');
 }
-
-console.log(criarCadastro);    */
-
-
